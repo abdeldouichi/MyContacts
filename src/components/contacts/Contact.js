130aa8a0fdb15a144023ad7d78ddc549bac4ec41
abdeldouichi <abdel.douichi@gmail.com>
2019-05-19 01:40:03 +0200

    first react component rcc

---
+++ src/components/contacts/Contact.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Contact : abdel douichi</h1>
+                <ul>
+                    <li>Tel: 06669541001</li>
+                    <li>Email: abdel.douichi@gmail.com</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Contact;
