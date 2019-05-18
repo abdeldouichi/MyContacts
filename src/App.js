a1e67d26d49748bc8d251ff5c93954b2e1ae9084
abdeldouichi <abdel.douichi@gmail.com>
2019-05-19 01:51:34 +0200

    first react --function-- component rfc

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Contact from './components/contacts/Contact';
+import NavBar from './components/shared/navbar/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Contact />
       <Contact />
       <Contact />
