3cd506fbba49956c0ab281968002372a1ec34d23
abdel <abdel.douichi@gmail.com>
2019-05-20 14:35:46 +0200

    change the state on component

--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -11,13 +11,21 @@ import DeleteIcon from '@material-ui/icons/Delete';
 
 
 class Contact extends Component {
+    state = {
+        showContactToggle: true
+    };
+
+
     showContact(myMessage) {
         console.log(myMessage);
-    }
+        this.setState({
+            showContactToggle: !this.state.showContactToggle,
+        })
+    };
 
     render() {
         let {data} = this.props;
-        let {id, name, tel, email} = data;
+        let { name, tel, email} = data;
 
         let colors = ["00A9D8", "0D9EDF", "259B9A", "292930", "3EB650", "FCC133", "E12B38", "7F8688", "929D9E", "F3E367", "18181E", "DEC79B", "F7F7F7", "7DA2A9", "4BFFA5", "040F3D", "C6C4C5", "34421E", "F1F1EF", "C19434", "FB8122", "1D2228", "E1E2E2", "D48166", "373A36", "", "E6E2DD", "151617", "090A0A", "151516", "000000", "181818", "2CCCC3", "FACD3D", "5626C4", "E60576", "32064A", "E56B1F", "FCD02C", "E42C6A"];
         return (
@@ -30,36 +38,53 @@ class Contact extends Component {
                         alt={name.toLowerCase()}/>
 
                     <div className="col-7 my-auto mr-0 ml-auto">
-                        <KeyboardArrowDown className="b-icon text-primary"
-                                           onClick={this.showContact.bind(this, name)}/>
-                        <DeleteIcon className="b-icon text-danger"/>
+                        <div className="col-7 my-auto mr-0 ml-auto">
+                            {
+                                (this.state.showContactToggle)
+                                    ? (
+                                        <KeyboardArrowDown className="b-icon text-primary"
+                                                           onClick={this.showContact.bind(this, name)}/>
+                                    )
+                                    : (
+                                        <KeyboardArrowUp className="b-icon text-primary"
+                                                         onClick={this.showContact.bind(this, name)}/>
+                                    )
+                            }
+                            <DeleteIcon className="b-icon text-danger"/>
+                        </div>
                     </div>
                 </div>
-                <div className="card-body p-0 col-12">
-                    <h3 className="card-title ">{name}</h3>
-                    <div className="row">
-                        <div className="card-text col-4 text-left">Email:</div>
-                        <a className="card-link col-8 text-left p-0" href={"mailto:" + email}>{email}</a>
+                <div className="card-body p-2 col-12">
+                    <h3 className="card-title text-right">{name}</h3>
+                    {
+                        (this.state.showContactToggle)
+                            ? (<div className="row">
+                                    <div className="card-text col-4 text-left">Email:</div>
+                                    <a className="card-link col-8 text-left p-0" href={"mailto:" + email}>{email}</a>
 
-                        <div className="card-text col-4 text-left">Tel:</div>
-                        <a className="card-link col-8 text-left p-0" href={"tel:" + tel}>{tel}</a>
-                    </div>
+                                    <div className="card-text col-4 text-left">Tel:</div>
+                                    <a className="card-link col-8 text-left p-0" href={"tel:" + tel}>{tel}</a>
+                                </div>
+                            )
+                            : null
+                    }
                 </div>
+
             </div>
         );
-    }
 
+    }
 }
 
 Contact.defaultProps = {
     data: null
-}
+};
 
 /*System  validation  proprieties*/
 Contact.propTypes = {
     id: PropTypes.number,
     data: PropTypes.object
-}
+};
 
 export default Contact;
 
