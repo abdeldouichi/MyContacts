9abcab9dbbdea21f36af93f777f6794628d2837e
abdel <abdel.douichi@gmail.com>
2019-05-22 11:40:11 +0200

    add reducer (Add_CONTACT)

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Consumer} from '../Context';
 
 class AddContact extends Component {
     state = {
@@ -7,43 +8,65 @@ class AddContact extends Component {
         phone: ''
     };
     onChangeInput = e => this.setState({[e.target.name]: e.target.value});
-    submit = e => {
+    submit = (dispatch,size, e) => {
         e.preventDefault();
-        console.log(this.state);
+        dispatch({
+            type: 'ADD_CONTACT',
+            payload: {
+                id: size+1,
+                name: this.state.name,
+                email: this.state.email,
+                tel: this.state.phone,
+            }
+        });
     };
+
     render() {
         const {name, email, phone} = this.state;
+
         return (
-            <div>
-                <form onSubmit={this.submit}>
-                    <div className="card">
-                        <div className="card-body">
-                            <h4 className="card-title">Add Contact</h4>
-                            <div className="card-body">
-                                <div className="form-group">
-                                    <label htmlFor="">Name</label>
-                                    <input name="name" type="text" className="form-control" defaultValue={name}
-                                           onChange={this.onChangeInput}
-                                    />
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="">Email</label>
-                                    <input name="email" type="text" className="form-control" defaultValue={email}
-                                           onChange={this.onChangeInput}
-                                    />
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="">Phone</label>
-                                    <input name="phone" type="text" className="form-control" defaultValue={phone}
-                                           onChange={this.onChangeInput}
-                                    />
-                                </div>
-                                <button className="btn btn-primary btn-block">Add new Contact</button>
+            <Consumer>
+                {
+                    value => {
+                        const {dispatch} = value;
+                        return (
+                            <div>
+                                <form onSubmit={this.submit.bind(this, dispatch, value.contacts.length)}>
+                                    <div className="card">
+                                        <div className="card-body">
+                                            <h4 className="card-title">Add Contact</h4>
+                                            <div className="card-body">
+                                                <div className="form-group">
+                                                    <label htmlFor="">Name</label>
+                                                    <input name="name" type="text" className="form-control"
+                                                           defaultValue={name}
+                                                           onChange={this.onChangeInput}
+                                                    />
+                                                </div>
+                                                <div className="form-group">
+                                                    <label htmlFor="">Email</label>
+                                                    <input name="email" type="text" className="form-control"
+                                                           defaultValue={email}
+                                                           onChange={this.onChangeInput}
+                                                    />
+                                                </div>
+                                                <div className="form-group">
+                                                    <label htmlFor="">Phone</label>
+                                                    <input name="phone" type="text" className="form-control"
+                                                           defaultValue={phone}
+                                                           onChange={this.onChangeInput}
+                                                    />
+                                                </div>
+                                                <button className="btn btn-primary btn-block">Add new Contact</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
                             </div>
-                        </div>
-                    </div>
-                </form>
-            </div>
+                        )
+                    }
+                }
+            </Consumer>
         );
     }
 }
