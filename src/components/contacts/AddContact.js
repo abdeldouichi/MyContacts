d4656d961a10ecd88d21b637d8a916906e298f11
abdel <abdel.douichi@gmail.com>
2019-05-22 10:26:08 +0200

    change state value by changing inputs

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,36 +1,48 @@
 import React, {Component} from 'react';
 
 class AddContact extends Component {
-    state ={
-        name :'',
-        email:'',
-        phone:''
+    state = {
+        name: '',
+        email: '',
+        phone: ''
+    };
+    onChangeInput = e => this.setState({[e.target.name]: e.target.value});
+    submit = e => {
+        e.preventDefault();
+        console.log(this.state);
     };
     render() {
-        const {name, email, phone } = this.state;
+        const {name, email, phone} = this.state;
         return (
             <div>
-                <div className="card">
-                    <div className="card-body">
-                        <h4 className="card-title">Add Contact</h4>
+                <form onSubmit={this.submit}>
+                    <div className="card">
                         <div className="card-body">
-                            <div className="form-group">
-                                <label htmlFor="">Name</label>
-                                <input type="text" className="form-control"  value={name}/>
+                            <h4 className="card-title">Add Contact</h4>
+                            <div className="card-body">
+                                <div className="form-group">
+                                    <label htmlFor="">Name</label>
+                                    <input name="name" type="text" className="form-control" defaultValue={name}
+                                           onChange={this.onChangeInput}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="">Email</label>
+                                    <input name="email" type="text" className="form-control" defaultValue={email}
+                                           onChange={this.onChangeInput}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="">Phone</label>
+                                    <input name="phone" type="text" className="form-control" defaultValue={phone}
+                                           onChange={this.onChangeInput}
+                                    />
+                                </div>
+                                <button className="btn btn-primary btn-block">Add new Contact</button>
                             </div>
-                            <div className="form-group">
-                                <label htmlFor="">Email</label>
-                                <input type="text" className="form-control" value={email}/>
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="">Phone</label>
-                                <input type="text" className="form-control" value={phone}/>
-                            </div>
-                            <button className="btn btn-primary btn-block">Add new Contact</button>
                         </div>
                     </div>
-                </div>
-
+                </form>
             </div>
         );
     }
